910b3e54107ec1525ca4972356d31013b42855dc
bodhiintellect <nath.syam.1986@gmail.com>
2025-02-22 15:01:23 +0530

    base url change

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Home-stay-booking/', // Set the base URL to match your GitHub Pages URL
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
