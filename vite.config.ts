6e979824d05752b349653f714f9ca94cfd895abd
bodhiintellect <nath.syam.1986@gmail.com>
2025-02-22 15:27:31 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+base: '/Home-stay-booking/', // Or your repository name if different.
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
